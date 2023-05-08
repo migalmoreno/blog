7a3ee256a49600f1973130c3ae42ca39cb6b560a
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-05-09 00:20:12 +0200

    feat: Modularize client-side logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
-const yearContainer = document.querySelector('#year')
-const currDate = new Date()
-yearContainer.innerHTML = currDate.getFullYear()
+function setCopyrightYear() {
+  document.querySelector("#year").innerHTML = new Date().getFullYear()
+}
+
+function highlightCurrentRoute() {
+  const rx = /\/+$/
+  for (let el of document.querySelectorAll("nav ul a")) {
+    if (el.href.replace(rx, "") === window.location.href.replace(rx, "")) {
+      el.classList.add("current")
+    }
+  }
+}
+
+function init() {
+  setCopyrightYear()
+  highlightCurrentRoute()
+}
+
+init()
