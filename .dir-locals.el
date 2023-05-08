0e5f27715b70d0eb2b4d90e3e95be5621813c62e
Miguel Ángel Moreno <mail@migalmoreno.com>
2023-05-09 00:19:11 +0200

    chore: Make fill column apply to all buffers

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((scheme-mode . ((fill-column . 80))))
+((nil . ((fill-column . 80))))
