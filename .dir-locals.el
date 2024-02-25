ead599c16ac9f98b36bde1b6bb9980fc2251e67b
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-02-25 12:02:36 +0100

    chore: add local compile command

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((fill-column . 80))))
+((nil . ((fill-column . 80)
+         (eval . (setq-local compile-command "make publish")))))
